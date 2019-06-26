6afd8b5a63c66265c131e14319b0d9fdce3233c3
Jeremy Morales <jeremy.morales@gmail.com>
2019-06-25 20:25:07 -0700

    swapi complete

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,37 @@
-import /* we need our action types here*/ "../actions";
+import {
+  FETCH_CHARS_START,
+  FETCH_CHARS_SUCCESS,
+  FETCH_CHARS_FAILURE
+} from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  error: "",
+  isFetching: false
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
+  console.log("reducer", action);
+
   switch (action.type) {
+    case FETCH_CHARS_START:
+      return {
+        ...state,
+        isFetching: true
+      };
+    case FETCH_CHARS_SUCCESS:
+      console.log(action.payload);
+      return {
+        ...state,
+        characters: [ ...state.characters, ...action.payload],
+        isFetching: false,
+        error: false
+      };
+    case FETCH_CHARS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
